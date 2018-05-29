09276dc7c8174844828d0be8c3e6e307ae9cc4f0
Per <dubgeiser@gmail.com>
2018-05-29 13:50:32 +0200

    Refactor logfocus.py a bit:
    Some extract methods.
    Sleep time is now configurable.
    Adhere to PEP8 more.

--- bin/logfocus.py
+++ bin/logfocus.py
@@ -14,19 +14,44 @@ This needs to be run with the standard Apple-installed Python because we need
 AppKit.
 '''
 
-from AppKit import NSWorkspace
 from datetime import datetime
 from time import sleep
 
-last_active_name = None
+from AppKit import NSWorkspace
 
-while True:
-    active_app = NSWorkspace.sharedWorkspace().activeApplication()
-    if active_app['NSApplicationName'] != last_active_name:
-        last_active_name = active_app['NSApplicationName']
-        print('%s: %s [%s]' % (
-            datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            active_app['NSApplicationName'],
-            active_app['NSApplicationPath']
-            ))
-    sleep(1)
+
+#
+# configuration:
+#   - sleep_time        Amount of seconds to sleep before rechecking app change.
+#
+conf = {
+    'sleep_time' : 1,
+}
+
+def print_app_data(app):
+    print('%s: %s [%s]' % (
+        datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        app['NSApplicationName'],
+        app['NSApplicationPath']
+        ))
+
+
+def track_app_focus_change(sleep_time):
+    last_active_name = None
+    while True:
+        active_app = NSWorkspace.sharedWorkspace().activeApplication()
+        if active_app['NSApplicationName'] != last_active_name:
+            last_active_name = active_app['NSApplicationName']
+            print_app_data(active_app)
+        sleep(sleep_time)
+
+
+def main(conf):
+    try:
+        track_app_focus_change(conf['sleep_time'])
+    except KeyboardInterrupt, e:
+        print "\nDone.\n"
+
+
+if __name__ == '__main__':
+    main(conf)
